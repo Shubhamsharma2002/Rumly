e988660b444675a1c738549c9503fa549f84e8be
shubham sharma <shubhamtech004@gmail.com>
2025-10-27 22:58:09 +0530

    push

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,13 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const RightSidebar = () => {
-  return (
-    <div>
-      
-      RightSidebar
-      
-      </div>
-  )
-}
+  return <div>RightSidebar</div>;
+};
 
-export default RightSidebar
+export default RightSidebar;
