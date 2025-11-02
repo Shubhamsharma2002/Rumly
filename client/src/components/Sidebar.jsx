a575c1cc911cbb71504efda5b2147b10e3040c7c
shubham sharma <shubhamtech004@gmail.com>
2025-11-02 23:18:25 +0530

    testing

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 const Sidebar = ({ slectedUser, setSelectedUser }) => {
   const navigate = useNavigate();
   return (
-    
     <div
       className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${
         setSelectedUser ? "max-md:hidden" : ""
